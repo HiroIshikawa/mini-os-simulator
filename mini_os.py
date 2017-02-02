30c81240a4d4840dd5b01d48ba4e943dbed0ee73
HiroIshikawa <hiro52ishikawa@gmail.com>
2017-02-02 02:36:18 -0800

    Add unit test suite, driver for init and create command

--- mini_os.py
+++ mini_os.py
@@ -4,6 +4,21 @@ import sys
 def initiate():
 	pass
 
+def create():
+	pass
+
+def destroy():
+	pass
+
+def request():
+	pass
+
+def release():
+	pass
+
+def timeout():
+	pass
+
 def is_int(s):
 	try:
 		int(s)
@@ -23,13 +38,13 @@ def parse(input):
 	if not args:
 		return 'no input'
 	# command
-	if (args[0]=='init'):
+	if args[0]=='init':
 		if (len(args) >= 2):
 			return 'init: no argument allowed'
 		initiate()
 
-	if (args[0]=='cr'):
-		if len(args) <= 2:
+	if args[0]=='cr':
+		if len(args) <= 3:
 			return 'create: need two arguments, name and priority'
 		if len(args) >= 4:
 			return 'create: too many arguments'
@@ -41,6 +56,49 @@ def parse(input):
 			return 'create: priority should be integer'
 		if (int(args[2]) <= 0 or int(args[2]) >= 3):
 			return 'create: priority should be 1 or 2'
+		create()
+	elif args[0]=='de':
+		if len(args) <= 2:
+			return 'destroy: need one argument, name'
+		if len(args) >= 3:
+			return 'destroy: too many arguments'
+		if is_int(args[1]):
+			return 'destroy: name should be one char'
+		if len(args[1]) >= 2:
+			return 'destroy: name should be one char'
+		destroy()
+	elif args[0]=='req':
+		if len(args) <= 3:
+			return 'reqeust: need two arguments, name and priority'
+		if len(args) >= 4:
+			return 'reqeust: too many arguments'
+		if is_int(args[1]):
+			return 'reqeust: resource name should be two char'
+		if len(args[1]) >= 3:
+			return 'reqeust: resource name should be two char'
+		if not is_int(args[2]):
+			return 'reqeust: priority should be integer'
+		if (int(args[2]) <= 0 or int(args[2]) >= 3):
+			return 'reqeust: priority should be 1 or 2'
+		request()
+	elif args[0]=='rel':
+		if len(args) <= 3:
+			return 'release: need two arguments, name and priority'
+		if len(args) >= 4:
+			return 'release: too many arguments'
+		if is_int(args[1]):
+			return 'release: resource name should be two char'
+		if len(args[1]) >= 3:
+			return 'release: resource name should be two char'
+		if not is_int(args[2]):
+			return 'release: priority should be integer'
+		if (int(args[2]) <= 0 or int(args[2]) >= 3):
+			return 'release: priority should be 1 or 2'
+		release()
+	elif args[0]=='to':
+		if len(args) >= 2:
+			return 'time-out: no argument allowed'
+		timeout()
 
 	# argument
 	parsed_input = input
