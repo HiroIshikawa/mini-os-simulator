6176bcae0a1360ba9c869290e8cf4f7b7fa6b7d6
HiroIshikawa <hiro52ishikawa@gmail.com>
2017-03-12 16:54:11 -0700

    Initial impl of VA translation process

--- project3/test.py
+++ project3/test.py
@@ -1,10 +1,54 @@
+from sys import argv
 
-# reading segments and address (s, f)
-# raading page, segments, and address (p, s, f)
+import vm
+
+input_file1 = argv[1]
+input_file2 = argv[2]
+
+with open(input_file1) as f:
+	lines = f.readlines()
+
+# split inputs to st input and pt input with mapping all values to ints
+st_init = map(int, lines[0].split(' '))
+pt_init = map(int, lines[1].split(' '))
+
+# print st_init
+# print pt_init
+
+# split st input into the format of pairs 
+st_init_pairs = []
+for i in range(len(st_init)):
+	if i%2 != 0:
+		st_init_pairs.append((st_init[i-1], st_init[i]))
+
+# print st_init_pairs
+
+# split pt input into to format of triples
+pt_init_triples = []
+for i in range(1,len(pt_init)+1):
+	if i > 1 and i%3 == 0:
+		pt_init_triples.append((pt_init[i-3], pt_init[i-2], pt_init[i-1]))
+
+# print pt_init_triples
+
+pm = vm.initialize_pm(st_init_pairs, pt_init_triples)
 
 
-# raeding the first line of the 
-def init_VM():
-	# first line: s_1 f_1 ... s_n f_n
-	# segmentation index and the physical address
-	
+with open(input_file2) as f:
+	lines = f.readlines()
+
+# split inputs to st input and pt input with mapping all values to ints
+va_translate = map(int, lines[0].split(' '))
+
+# print st_init
+# print pt_init
+
+# split st input into the format of pairs 
+va_translate_pairs = []
+for i in range(len(va_translate)):
+	if i%2 != 0:
+		va_translate_pairs.append((va_translate[i-1], va_translate[i]))
+
+# print va_translate_pairs
+
+vm.translate_vm(pm, va_translate_pairs)
--- project3/vm.py
+++ project3/vm.py
@@ -44,12 +44,41 @@ class BitMap():
 			self.bitmap[ith] = self.bitmap[ith] & self.mask_0[offset]
 		except IndexError:
 			print 'List index out of range. Nothing changed'
+
+	def resides(self,i):
+		offset = i % self.num_blocks
+		ith  = i / self.num_blocks
+		return self.bitmap[ith+offset]
+
+	def first_fit_one(self):
+		for i,e in enumerate(self.bitmap):
+			for j,b in enumerate(mask_1):
+				if e & b:
+					return (i*self.block_size) + j
+		return None
+
+	def first_fit_two(self):
+		for i,e in enumerate(self.bitmap):
+			for j,b in enumerate(mask_1):
+				if e & b:
+					if j == len(mask_1)-1:
+						if not i == self.num_blocks-1:
+							if bitmap[i+1] & mask_1[0]:
+								return (i*self.block_size) + j
+					else:
+						if e & mask_1[j+1]:
+							return (i*self.block_size) + j
+
+
+		return None
 	
 	def print_bin(self):
 		"""Print the bitmap in binary format for integer data type (32bits)"""
 		print 'bitmap:'
-		for e in self.bitmap:
-			print '{0:032b}'.format(e),
+		# print len(self.bitmap)
+		for i,e in enumerate(self.bitmap):
+			print '{}'.format(i).rjust(3),
+			print '{0:032b}'.format(e)
 
 class Frame():
 	"""
@@ -59,6 +88,21 @@ class Frame():
 		entries: physical memory address assigned in the particular frame
 	"""
 
+	def __init__(self, size=512):
+		self.entries = [0]*size
+
+class SegmentationTable():
+	"""	Representation of the Segmentation Table (ST) in frame """
+	def __init__(self, size=512):
+		self.entries = [0]*size
+
+class PagingTable():
+	"""	Representation of the Pagin Table (ST) in frame """
+	def __init__(self, size=1024):
+		self.entries = [0]*size
+
+class Page():
+	""" Representation of the Page in frame"""
 	def __init__(self, size=512):
 		self.entries = [0]*size
 
@@ -72,7 +116,7 @@ class PhysicalMemory():
 		st_size: how many frames a Segmentation Table occupies in physical mem
 		pt_size: how many frmaes a Page Table occupies in physical mem
 		pg_size: how many frames a page occupies in physical memory
-		pm: array of the representationo of physical memory with configuration above
+		frames: array of the representationo of physical memory with configuration above
 		bitmap: bitmap manages whether a frame is free/occupied
 	"""
 	def __init__(self, num_frames=1024, frame_size=512, st_size=1, pt_size=2, pg_size=1):
@@ -81,22 +125,11 @@ class PhysicalMemory():
 		self.st_size = 1
 		self.pt_size = 2
 		self.pg_size = 1
-		self.pm = [None]*num_frames  # 1024 frames * 512 words / frame = 524,288 ints
-		self.pm[0] = Frame()  # the sgmentation table resides [0] always
+		self.frames = [None]*num_frames  # 1024 frames * 512 words / frame = 524,288 ints
+		self.frames[0] = SegmentationTable()  # the sgmentation table resides [0] always
 		self.bitmap = BitMap()
 		self.bitmap.occupy(0)  # register the ocuupation of [0]
 
-class SegmentationTable:
-	"""
-	Segmentation Table implementation
-
-	Attributes:
-		entries: each entry points to pagin table
-	"""
-
-	def __init__(self, st_size=512):
-		self.st_size = st_size
-		self.entries = [0]*self.st_size
 
 class VirtualMemory():
 	"""
@@ -108,3 +141,150 @@ class VirtualMemory():
 	"""
 	def __init__(self):
 		self.vm = [0]*512
+
+def initialize_pm(st_input, pt_input):
+	pm = PhysicalMemory()
+
+	# print pm.bitmap.print_bin()
+	
+	# make entries in ST
+	for st_pair in st_input:
+		try:
+			pm.frames[0].entries[st_pair[0]] = st_pair[1]
+		except IndexError:
+			print 'Out of bounds for the ST size (0-511)'
+		st_id = (pm.frames[0].entries[st_pair[0]] / pm.frame_size)
+		if not pm.bitmap.resides(st_id) > 0:  # check if the frame is allocatable
+			pm.frames[st_id] = PagingTable()
+			for i in range(pm.pt_size):
+				pm.bitmap.occupy(st_id+i)  # make sure the residesnts registered
+	print pm.frames[0].entries
+
+	# make entries in PT
+	for pt_triple in pt_input:
+		if pm.frames[0].entries[pt_triple[1]] > 0:  # the segmentation table entry resides?
+			st_id = (pm.frames[0].entries[pt_triple[1]] / pm.frame_size)  # idetify the frame
+			if not pm.bitmap.resides(st_id) > 0:  # check if the frame is allocatable
+				pm.frames[st_id].entries[pt_triple[0]] = pt_triple[2]  # assign
+				
+				if pt_triple[2] > -1: 
+					pt_id = (pt_triple[2] / pm.frame_size)
+					if not pm.bitmap.resides(pt_id) > 0:
+						pm.frames[pt_id] = Page()
+					for i in range(pm.pg_size):
+						pm.bitmap.occupy(pt_id+i)  # make sure the residesnts registered
+	print pm.frames[4].entries
+
+	pm.bitmap.print_bin()
+	return pm
+
+def purse_va(va):
+	va = '{0:032b}'.format(va)
+	# print va
+	# print bits
+	# lead = va[0:4]
+	lead = int(va[0:4],2)
+	# st   = va[4:13]
+	st   = int(va[4:13],2)
+	# pt   = va[13:23]
+	pt   = int(va[13:23],2)
+	# pg   = va[23:32]
+	pg   = int(va[23:32],2)
+	print lead, st, pt, pg
+	return st, pt, pg
+
+def translate_vm(pm, va_inputs):
+	for va_input in va_inputs:
+		op = va_input[0]
+		va = va_input[1]
+		st, pt, pg = purse_va(va)
+		if op == 0:
+			#read
+			if pm.frames[0].entries[st] == -1:
+				print 'pf',
+			elif pm.frames[0].entries[st] == 0:
+				print 'err',
+			else:
+				pt_id = pm.frames[0].entries[st] / pm.frame_size
+				if pm.frames[pt_id].entries[pt] == -1:
+					print 'pf',
+				elif pm.frames[pt_id].entries[pt] == 0:
+					print 'err',
+				else:
+					print pm.frames[pt_id].entries[pt], pg 
+					pa = pm.frames[pt_id].entries[pt] + pg
+					print pa,
+		elif op == 1:
+			# write
+			# when new allocation rquird, first-fit is fine
+			# the choice of algorithms should not affect the correctness of the program
+			if pm.frames[0].entries[st] == -1:
+				print 'pf',
+			if pm.frames[0].entries[st] > 0:
+				pt_id = pm.frames[0].entries[st] / pm.frame_size
+				if pm.frames[pt_id].entires[pt] == -1:
+					print 'pf'
+			pt_id = pm.frames[0].entries[st] / pm.frame_size
+			# if ST entry is 0
+			if pm.frames[0].entries[st] == 0:
+				# check if there's allocatable frame for a Page Table
+				available_frame = pm.bitmap.first_fit_two()
+				if available_frame:  # if so, make a new paging table
+					pm.frames[available_frame] = PagingTable()
+					for i in range(pm.pt_size):  # register it in bitmap
+						pm.bitmap.occupy(pt_id+i)
+					pm.frames[0].entries[st] = available_frame*pm.frame_size
+				else:  # if there's no available frame, no more process
+					print 'No available for the PT'
+				# if PT entry is 0
+				if pm.frames[pt_id].entires[pt] == 0:
+					# PA = PM[ PM[s] + p ] + w
+					# check if there's allocatable frame for a Page
+					available_frame = pm.bitmap.first_fit()
+					if available_frame:  # if so, make a new page
+						pm.frames[available_frame] = Page()
+						for i in range(pm.pg_size):  # register it in bitmap
+							pm.bitmap.occupy()
+						pm.frames[pt_id].entries[pt] = available_frame*pm.frame_size
+					else:
+						print 'No avaliable for the Page'
+					# find the alocatable frame, 
+					# translate it into address format, 
+					# register it in corresponding pt entry
+				else:
+
+
+
+			# 	if pm.frames[pt_id].entries[pt] == -1:
+			# 		print 'pf',
+			# 	elif pm.frames[pt_id].entries[pt] == 0:
+			# 		print 'err',
+			# 	else:
+			# 		print pm.frames[pt_id].entries[pt], pg 
+			# 		pa = pm.frames[pt_id].entries[pt] + pg
+			# 		print pa,
+
+			pass
+		else:
+			pass
+		print ''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
